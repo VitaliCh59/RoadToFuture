37aad83f59bf9cec8299034557656a2e107ab514
Vitalich59 <chyrukaevva@gmail.com>
2020-10-21 06:38:44 +0500

    60lvl

--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -3,11 +3,16 @@ import Profile from "./Profile";
 import * as axios from "axios";
 import {connect} from "react-redux";
 import {setUserProfile} from "../../redux/profile-reducer";
+import {withRouter} from "react-router-dom";
 
 class ProfileContainer extends React.Component {
 
     componentDidMount() {
-        axios.get(`https://social-network.samuraijs.com/api/1.0/profile/2`)
+        let userId = this.props.match.params.userId;
+        if (!userId)  {
+            userId = 12053;
+        }
+        axios.get(`https://social-network.samuraijs.com/api/1.0/profile/`+userId)
             .then(responce => {
                 this.props.setUserProfile(responce.data);
 
@@ -25,4 +30,9 @@ class ProfileContainer extends React.Component {
 let mapStateToProps = (state) => ({
     profile: state.profilePage.profile})
 
-export default  connect (mapStateToProps, {setUserProfile}) (ProfileContainer);
+let WithUrlDataContainerComponent = withRouter(ProfileContainer);
+// виз роутер отрисовывает новую компоненту как коннект, но добавляет данные из урла+стора
+
+
+export default  connect (mapStateToProps, {setUserProfile}) (WithUrlDataContainerComponent);
+// коннект через запросы получает данные(коллбэки) из стора. Он отрисовывает новую компоненту  данными из стора
