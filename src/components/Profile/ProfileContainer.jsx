9e2d3fc9e7f996f81947dfdd2b9f038f0cf84b07
Vitalich59 <chyrukaevva@gmail.com>
2020-12-17 06:20:43 +0500

    96lvl

--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -1,36 +1,48 @@
 import React from "react";
 import Profile from "./Profile";
 import {connect} from "react-redux";
-import {getStatus, getUserProfile, updateStatus} from "../../redux/profile-reducer";
-import {Redirect, withRouter} from "react-router-dom";
-import {withAuthRedirect} from "../../hoc/withAuthRedirect";
+import {getStatus, getUserProfile, savePhoto, updateStatus} from "../../redux/profile-reducer";
+import {withRouter} from "react-router-dom";
 import {compose} from "redux";
 
 
 class ProfileContainer extends React.Component {
 
-    componentDidMount() {
+    refreshProfile() {
         let userId = this.props.match.params.userId;
-        if (!userId)  {
+        if (!userId) {
             userId = this.props.authorizedUserId;
             if (!userId) {
-                this.props.history.push ('/login')
+                this.props.history.push('/login')
             }
         }
         this.props.getUserProfile(userId);
         this.props.getStatus(userId);
     }
 
+    componentDidMount() {
+        this.refreshProfile();
+    }
+// нужен чтоб при нажатии на профиль (если мы находимся на чужом профиле) появлялся свой профиль
+    componentDidUpdate(prevProps, prevState, snapshot) {
+       if (this.props.match.params.userId !== prevProps.match.params.userId ) {
+           this.refreshProfile()
+       }
+    }
+
     render() {
 
         return (
-           <Profile {...this.props}
-                    profile ={this.props.profile}
-                    status = {this.props.status}
-                    updateStatus = {this.props.updateStatus} />
-           );
+            <Profile {...this.props}
+                     isOwner = {!this.props.match.params.userId}
+                     profile={this.props.profile}
+                     status={this.props.status}
+                     savePhoto={this.props.savePhoto}
+                     updateStatus={this.props.updateStatus}/>
+        );
     };
 }
+
 let mapStateToProps = (state) => ({
     profile: state.profilePage.profile,
     status: state.profilePage.status,
@@ -53,8 +65,8 @@ export default  connect (mapStateToProps, {getUserProfile}) (WithUrlDataContaine
 
 
 // всё, что выше обернём в функцию компоус
-export default compose (
-    connect (mapStateToProps, {getUserProfile, getStatus, updateStatus}),
+export default compose(
+    connect(mapStateToProps, {getUserProfile, getStatus, savePhoto, updateStatus}),
     withRouter
     //withAuthRedirect
 )(ProfileContainer);
--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,28 +1,35 @@
 import React from "react";
-import y from "./ProfileInfo.module.css";
+import s from "./ProfileInfo.module.css";
 import Preloader from "../../common/Preloader/Preloader";
 import ProfileStatusWithHooks from "./ProfileStatusWithHooks";
+import userPhoto from "../../../assets/images/img1.jpg";
 
-const ProfileInfo = ({profile, status, updateStatus, ...props}) => {
+const ProfileInfo = ({profile, status, updateStatus, isOwner, savePhoto, ...props}) => {
     if (!profile /*props.profile == null || props.profile  Звучит, как если у нас нет профайла*/) {
-        return <Preloader />
+        return <Preloader/>
+    }
+    //хватаемся за файл, который вставим в импут с помощью онЧендж
+    const onMainPhotoSelected = (e) => {
+        if (e.target.files.length) {
+            savePhoto(e.target.files[0])
+        }
     }
 
-  return (
-    <div>
-      <div>
-        <img src={process.env.PUBLIC_URL + "/ducati.jpeg"} alt="ducati" />
-      </div>
-      <div>
-        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcR_6UP6TWFDTFZvLjcy6Rq8xnN130gpslTjww&usqp=CAU"></img>
-      </div>
-      <div className={y.descriptionBlock}>
-          <ProfileStatusWithHooks status={status} updateStatus={updateStatus}/>
-          <img src = {profile.photos.large}/>
+    return (
+        <div>
+            <div>
+                <img src={process.env.PUBLIC_URL + "/ducati.jpeg"} alt="ducati"/>
+            </div>
 
-      </div>
-    </div>
-  );
+            <div className={s.descriptionBlock}>
+                <img src={profile.photos.large || userPhoto} className={s.mainPhoto}/>
+                {isOwner && <input type={"file"} onChange={onMainPhotoSelected}/>}
+                <ProfileStatusWithHooks status={status} updateStatus={updateStatus}/>
+            </div>
+        </div>
+    );
 };
 
 export default ProfileInfo;
+
+// <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcR_6UP6TWFDTFZvLjcy6Rq8xnN130gpslTjww&usqp=CAU"></img>
